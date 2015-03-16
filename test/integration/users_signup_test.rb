cce1d44d14d7577907dc7166483ea96837d2c328
jobenscott <your.jobenscott@yahoo.com>
2015-03-16 11:41:18 -0600

    smaller picture

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,5 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.FILL_IN
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
 end
